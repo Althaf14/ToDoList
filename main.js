7fed28bec9e0528987ce2915e08dd097da1707f2
Althaf14 <althaf1426@gmail.com>
2024-09-21 21:28:04 +0530

    initial

---
+++ main.js
@@ -0,0 +1,16 @@
+let parentDiv = document.getElementById("parent-div");
+let itemInput = document.getElementById("item-input");
+let addBtn = document.getElementById("add-btn");
+let itemList = document.getElementById("item-ul");
+
+function addItems(){
+    let itemInputData = itemInput.value;
+    let li = document.createElement("li");
+    li.innerText = itemInputData;
+    itemList.appendChild(li)
+    itemInput.value =""
+    itemInput.focus();
+    console.log(li);
+}
+
+addBtn.addEventListener("click",addItems)
