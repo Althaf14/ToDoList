8d7ff4611b3dfc8eddcf51269baab21e8c77e6f2
Althaf14 <althaf1426@gmail.com>
2024-12-24 15:00:59 +0530

    local storage

--- main.js
+++ main.js
@@ -2,9 +2,20 @@ let parentDiv = document.getElementById("parent-div");
 let itemInput = document.getElementById("item-input");
 let addBtn = document.getElementById("add-btn");
 let itemList = document.getElementById("item-ul");
+let itemInputData
 
-function addItems(){
-    let itemInputData = itemInput.value;
+function setItems(){
+    localStorage.setItem("itemName",itemInputData)
+}
+
+function getItems(){
+    if(localStorage.getItem("itemName")){
+    itemInputData=localStorage.getItem("itemName")
+    ui();
+    }
+}
+
+function ui(){
     let li = document.createElement("li");
     li.innerText = itemInputData;
     itemList.appendChild(li)
@@ -13,7 +24,25 @@ function addItems(){
     let trashBtn = document.createElement('i');
     trashBtn.classList.add('fa-solid','fa-trash');
     li.appendChild(trashBtn);
+
+}
+
+function addItems(){
+    itemInputData = itemInput.value;
+
+    setItems();
+
+    getItems();
+ 
     
+}
+
+function deleteItems(event){
+    if(event.target.classList[0]=== "fa-solid"){
+        let item = event.target.parentElement;
+        item.remove();
+    }
+
 }
 
 addBtn.addEventListener("click",addItems)
@@ -23,3 +52,6 @@ document.addEventListener('keypress', function(event) {
 	    itemInput.value = "";
 	}
 });
+
+itemList.addEventListener('click',deleteItems)
+getItems();
